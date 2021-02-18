3082973ff947f48e4ed2a43458872b3f04698fb0
irineltudor <irineltudor@gmail.com>
2021-02-18 14:56:23 +0200

    Added validate

--- Lab1/src/com/Lab1/optional/Main.java
+++ Lab1/src/com/Lab1/optional/Main.java
@@ -1,26 +1,42 @@
 package com.Lab1.optional;
 
 public class Main {
-
+    /*
+    Check if the argument is valid
+     */
+    public static boolean isValid(double number)
+    {
+        return Math.floor(number) == number && number > 0;
+    }
     public static void main(String[] args) {
 
-        int length =10;
-        GRAPH firstGraph= new GRAPH(length);
-        firstGraph.randomGraph();
-        firstGraph.printMatrix();
-        firstGraph.connectedComponentsFunction();
+        double number =Integer.parseInt(args[0]);
+        if(isValid(number))
+        {  int length=(int)(number);
 
-        if(firstGraph.getNumberOfComponents()==1)
-        {
-            System.out.println("Graph connected");
-            firstGraph.createTreeMatrix();
-            firstGraph.printTreeMatrix();
+            GRAPH firstGraph= new GRAPH(length);
+            firstGraph.randomGraph();
+            firstGraph.printMatrix();
+            firstGraph.connectedComponentsFunction();
+
+            if(firstGraph.getNumberOfComponents()==1)
+            {
+                System.out.println("Graph connected");
+                firstGraph.createTreeMatrix();
+                firstGraph.printTreeMatrix();
+            }
+            else
+            {
+                System.out.println("Graph connected components:");
+                firstGraph.printConnectedComponents();
+
+            }
+
+
         }
         else
         {
-            System.out.println("Graph connected components:");
-            firstGraph.printConnectedComponents();
-
+            System.out.println("The argument is not valid!");
         }
 
 
